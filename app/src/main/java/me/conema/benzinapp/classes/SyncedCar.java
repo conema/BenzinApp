2bcae688b47de3141fb45532e6af8d5e99256ad5
Davide <hekishoku15@gmail.com>
2019-01-10 19:17:56 +0100

    Modular car List layout Added onClickListener to syncedCar saveButton. Sgherri vari.

--- app/src/main/java/me/conema/benzinapp/classes/SyncedCar.java
+++ app/src/main/java/me/conema/benzinapp/classes/SyncedCar.java
@@ -3,7 +3,10 @@ package me.conema.benzinapp.classes;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import me.conema.benzinapp.R;
 import me.conema.benzinapp.classes.Car;
@@ -11,6 +14,8 @@ import me.conema.benzinapp.classes.CarFactory;
 
 public class SyncedCar extends AppCompatActivity {
     EditText carText, roadText, fuelText;
+    ImageButton imgPicker;
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,33 @@ public class SyncedCar extends AppCompatActivity {
         carText = (EditText) findViewById(R.id.carEditText);
         fuelText = (EditText) findViewById(R.id.fuelEditText);
         roadText = (EditText) findViewById(R.id.roadEditText);
+        imgPicker = findViewById(R.id.imgButton);
+        saveButton = findViewById(R.id.saveButton);
+
         final CarFactory factory = CarFactory.getInstance();
         Car car = new Car(0, "Fiat punto", 55000, 17, null, Color.parseColor("#121212"), 70);
         carText.setText(car.getName());
         fuelText.setText("5");
         roadText.setText(car.getKmDone());
+
+        imgPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO prendere l'immagine
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String carName = carText.getText().toString();
+                int kmDone = Integer.parseInt(roadText.getText().toString());
+                Car car = new Car(0, carName, kmDone, 23, null, 123, 80);
+                factory.addCar(car);
+            }
+        });
+
+
 
     }
 }
